82c4ebee8fb3006ec13bd5974a89d4486e70e0d9
NikitaRR <nikita200993@gmail.com>
2021-11-14 14:05:04 +0300

    add run script

---
+++ ansible/run.sh
@@ -0,0 +1,15 @@
+set -e
+
+script_dir=$(dirname "$0")
+pid_file="$script_dir"/pid
+jar_file="$script_dir"/app.jar
+
+if [ -f "$pid_file" ]; then
+  childPid=$(cat "$pid_file")
+  if ps -p "$childPid" >/dev/null; then
+      pkill "$childPid"
+  fi
+fi
+
+java -jar "$jar_file" 9999
+echo "$!" > "$pid_file"
